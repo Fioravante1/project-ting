34c8ead186644e35e6a727a729f0b2ad00cccde9
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2022-02-20 20:26:45 -0300

    feat: add funcao txt importer - requisito dois

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+# https://stackoverflow.com/questions/5574702/how-to-print-to-stderr-in-python
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if not path_file.endswith('.txt'):
+            return sys.stderr.write('Formato inválido\n')
+        with open(path_file, 'r') as file:
+            return file.read().split('\n')
+    except FileNotFoundError:
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
