695d949b52fe6990745adc32be4b0512743ef16e
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2022-02-21 08:44:18 -0300

    feat: add funcao exists word - requisito seis

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,19 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    word_list = []
+    for index in range(len(instance.data)):
+        file = instance.search(index)
+        for line in file["linhas_do_arquivo"]:
+            repeat_lines = []
+            if word.lower() in line.lower():
+                repeat_lines.append({"linha": index + 1})
+
+    if repeat_lines:
+        word_list.append({
+            "palavra": word,
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": repeat_lines,
+        })
+    return word_list
 
 
 def search_by_word(word, instance):
