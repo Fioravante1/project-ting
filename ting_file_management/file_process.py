230200ee5e59aaf39e42e69b3b30c8d09babc7b0
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2022-02-20 21:47:51 -0300

    feat: add funcao process - requisito 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,21 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file = txt_importer(path_file)
+
+    for item in range(len(instance.data)):
+        if path_file == instance.search(item)["nome_do_arquivo"]:
+            return None
+
+    result = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file),
+        "linhas_do_arquivo": file,
+    }
+    instance.enqueue(result)
+    sys.stdout.write(str(result))
 
 
 def remove(instance):
